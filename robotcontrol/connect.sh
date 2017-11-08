627900dbc7e6d2651fd2438ddfc1b5899faf196e
Hyung Song <hyung.song@lge.com>
2017-11-08 22:20:30 +0000

    Update connect.sh to latest

--- robotcontrol/connect.sh
+++ robotcontrol/connect.sh
@@ -2,13 +2,25 @@
 
 RTOPIC=request
 WTOPIC=response
+HOST=localhost
 
 if (( $# < 2 )); then
-  echo "Usage: ${0} broker_address executable [...]"
+  echo "Usage: ${0} role executable [...]"
+  echo "       role : robot | algorithm"
+  echo "       executable : executable file + arguments"
+  exit 1
 fi
 
-mosquitto_sub -h ${1} -t ${RTOPIC} | ${@:2} | \
+if [[ ${1} != "robot" && ${1} != "algorithm" ]]; then
+  echo "Error: Unknown role name. Use robot or algorithm"
+  exit 1
+fi
+
+RTOPIC=${1}-request
+WTOPIC=${1}-response
+
+mosquitto_sub -h ${HOST} -t ${RTOPIC} | ${@:2} | \
 while read line; do
-  mosquitto_pub -h ${1} -t ${WTOPIC} -m "${line}"
+  mosquitto_pub -h ${HOST} -t ${WTOPIC} -m "${line}"
 done
 
